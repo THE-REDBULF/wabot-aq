f83012d79de5b4e5ada7a0cc93a0bc8b1319da59
THE-REDBULF <111446748+THE-REDBULF@users.noreply.github.com>
2022-08-21 12:39:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 require('./config.js')
 const { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
-this.version =[2,2119,6]
+this.version =[2,2143,3]
 const cloudDBAdapter = require('./lib/cloudDBAdapter')
 const { generate } = require('qrcode-terminal')
 const syntaxerror = require('syntax-error')
@@ -24,7 +24,7 @@ const { Low, JSONFile } = low
 const rl = Readline.createInterface(process.stdin, process.stdout)
 const WAConnection = simple.WAConnection(_WAConnection)
 //WHEN
-
+simple.version = [2,2143,3]
 
 
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
